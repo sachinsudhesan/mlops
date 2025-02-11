6b1459fe51b50fab61a22999b88e991e5809a8e2
sachin <sachin@reizend.ai>
2025-02-11 13:10:12 +0530

    s

--- src/model/model_evaluation.py
+++ src/model/model_evaluation.py
@@ -13,9 +13,20 @@ import os
 
 
 
+# Set up DagsHub credentials for MLflow tracking
+dagshub_token = os.getenv("DAGSHUB_PAT")
+if not dagshub_token:
+    raise EnvironmentError("DAGSHUB_PAT environment variable is not set")
+
+os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+
+dagshub_url = "https://dagshub.com"
+repo_owner = "sachinsudhesan"
+repo_name = "mlops"
+
 # Set up MLflow tracking URI
-mlflow.set_tracking_uri('https://dagshub.com/sachinsudhesan/mlops.mlflow')
-dagshub.init(repo_owner='sachinsudhesan', repo_name='mlops', mlflow=True)
+mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
 
 # logging configuration
 logger = logging.getLogger('model_evaluation')
